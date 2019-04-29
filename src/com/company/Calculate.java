39c90d16e312ea2baa359c90d1f6f3dc4b4bb3c5
Corwin <amfetaminx@gmail.com>
2019-04-29 16:15:20 +0300

    Command line calculator supporting Arabic and Roman numerals from 1 to 10 inclusive, and operations +, -, *, /.

--- src/com/company/Calculate.java
+++ src/com/company/Calculate.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Calculate {
-    private static Map<String, Arithmetic> operators;
+    private static Map<String, Arithmetic> operations;
     static {
-        operators = new HashMap<>();
-        operators.put("+", new Add());
-        operators.put("-", new Subtract());
-        operators.put("*", new Multiply());
-        operators.put("/", new Divide());
+        operations = new HashMap<>();
+        operations.put("+", new Add());
+        operations.put("-", new Subtract());
+        operations.put("*", new Multiply());
+        operations.put("/", new Divide());
     }
 
     public static abstract class Arithmetic { public abstract int calculate(int a, int b); }
@@ -21,6 +21,6 @@ class Calculate {
     public static class Divide extends Arithmetic { public int calculate(int a, int b) { return a / b; } }
 
     static int calculate (int a, int b, String operation) {
-        return operators.get(operation).calculate(a, b);
+        return operations.get(operation).calculate(a, b);
     }
 }
